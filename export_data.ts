39b0ea3a2528c881d77048ff69214b40a7395103
Krish Kavathia <krish@Krishs-MacBook-Air.local>
2026-03-29 11:04:26 +0530

    chore: clean up project structure by removing legacy scripts and data files

--- export_data.ts
+++
@@ -1,6 +0,0 @@
-import fs from "fs";
-import { products } from "./src/lib/products.ts";
-
-// Simple script to export TS data array to JSON for backend
-fs.writeFileSync("./server/data/products.json", JSON.stringify(products, null, 2));
-console.log("Successfully exported products data to JSON for the backend!");
